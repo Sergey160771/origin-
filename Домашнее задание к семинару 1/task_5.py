5d83fbba207307fe2af90931f69cdae816c79791
Sergy <9058591133@mail.ru>
2022-11-22 15:22:26 +0500

    Homework number 1

--- Домашнее задание к семинару 1/task_5.py
+++ Домашнее задание к семинару 1/task_5.py
@@ -5,6 +5,6 @@ coordinate_XA = float(input('Ведите координату X точки A: '
 coordinate_YA = float(input('Ведите координату Y точки A: '))
 coordinate_XB = float(input('Ведите координату X точки B: '))
 coordinate_YB = float(input('Ведите координату Y точки B: '))
-distance = abs((coordinate_XA - coordinate_XB)**2 + (coordinate_YA - coordinate_YB)**2)
+distance = round(((coordinate_XA - coordinate_XB)**2 + (coordinate_YA - coordinate_YB)**2)**0.5 ,3)
 print(f'Растояние от точки A до точки B = {distance}')
 
